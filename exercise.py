331bb67a6786c877d62a14e62a74deee750b7ee7
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-09-08 22:42:28 -0700

    Add exercise 5

--- exercise.py
+++ exercise.py
@@ -148,4 +148,65 @@ def weather_advice():
     elif is_cold == 'no' and is_raining == 'no':
         print('Wear light clothing.')
 # Call the function
-weather_advice()
+# weather_advice()
+
+# Exercise 5: What's the Season?
+#
+# Write a Python function named `determine_season` that figures out the season based on the entered date.
+#
+# Requirements:
+# - The function should first prompt the user to enter the month (as three characters): "Enter the month of the year (Jan - Dec):"
+# - Then, the function should prompt the user to enter the day of the month: "Enter the day of the month:"
+# - Determine the current season based on the date:
+#      - Dec 21 - Mar 19: Winter
+#      - Mar 20 - Jun 20: Spring
+#      - Jun 21 - Sep 21: Summer
+#      - Sep 22 - Dec 20: Fall
+# - Print the season for the entered date in the format: "<Mmm> <dd> is in <season>."
+#
+# Hints:
+# - Use 'in' to check if a string is in a list or tuple.
+# - Adjust the season based on the day of the month when needed.
+# - Ensure to validate input formats and handle unexpected inputs gracefully.
+
+def determine_season():
+    # Your control flow logic goes here
+    months = ('jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec')
+    days = (31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+    spring = ('mar', 'apr', 'may', 'jun')
+    summer = ('jun', 'jul', 'aug', 'sep')
+    fall = ('sep', 'oct', 'nov', 'dec')
+    winter = ('dec', 'jan', 'feb', 'mar')
+    season = ''
+    month = input('Enter the month of the year (Jan - Dec): ').lower()
+    while not month in months:
+        month = input('Invalid input. Please enter a month of the year by typing the first three letters of the month name (Jan - Dec): ')
+    day = input('Enter the day of the month: ')
+    while not day.isdecimal() or not int(day) in range(1, days[months.index(month)]):
+        day = input(f'Invalid input. There are {days[months.index(month)]} days in {month}. Please enter a whole positive number between 1 and {days[months.index(month)]}: ')
+    day = int(day)
+    while not season:
+        if month in spring:
+            if (month == 'mar' and not day in range(20, 32)) or (month == 'jun' and not day in range(1, 21)):
+                pass
+            else:
+                season = 'spring'
+                break
+        if month in summer:
+            if month == 'sep' and not day in range(1, 22):
+                pass
+            else:
+                season = 'summer'
+                break
+        if month in fall:
+            if month == 'dec' and not day in range(1, 21):
+                pass
+            else:
+                season = 'fall'
+                break
+        if month in winter:
+            season = 'winter'
+            break
+    print(f'{month} {day} is in {season}.')
+# Call the function
+determine_season()
